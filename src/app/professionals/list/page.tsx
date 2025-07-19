ffe840de5ff636e939708ec59e3c4dc581d1721c
PolyBecker <paulinehellinger@gmail.com>
2025-07-19 13:30:21 -0300

    margim 0 page

--- src/app/professionals/list/page.tsx
+++ src/app/professionals/list/page.tsx
@@ -59,7 +59,7 @@ export default function ProfessionalsListPage() {
 
 
   return (
-    <div className="min-h-screen bg-[#FDFDFB] px-4 py-6 flex flex-col space-y-6 mt-4 mx-4 pb-24">
+    <div className="min-h-screen bg-[#FDFDFB] px-4 py-6 flex flex-col space-y-0 mt-0 mx-0 pb-0">
 
 
       {/* Título e card de destaque fixos */}
