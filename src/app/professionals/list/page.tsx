b85839be208a1455873c99adc01fe4051ab1b5c2
PolyBecker <149532276+PolyBecker@users.noreply.github.com>
2025-07-19 13:21:29 -0300

    Fix footer overlay and background

--- src/app/professionals/list/page.tsx
+++ src/app/professionals/list/page.tsx
@@ -59,7 +59,7 @@ export default function ProfessionalsListPage() {
 
 
   return (
-    <div className="min-h-screen bg-[#FDFDFB] px-4 py-6 flex flex-col space-y-6 mt-4 mx-4">
+    <div className="min-h-screen bg-[#FDFDFB] px-4 py-6 flex flex-col space-y-6 mt-4 mx-4 pb-24">
 
 
       {/* Título e card de destaque fixos */}
